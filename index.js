73b40796ae8eef5292e92ceade654ab23253932a
Gregory Singleton <yadig69design@gmail.com>
2020-06-15 03:42:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ const happyHolidaysTo = (name) => `Happy holidays, ${name}!`;
 const happyHolidayTo = (holiday, name) => `Happy ${holiday}, ${name}!`;
 const holidayCountdown = (holiday, days) => `It's ${days} days until ${holiday}!`;
 
-function happyHolidays() {
-  console.log("Happy holidays!")
-}
-
-function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`
-}
-
-function happyCustomHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`
-}
+// function happyHolidays() {
+//   console.log("Happy holidays!")
+// }
+// 
+// function happyHolidaysTo(name) {
+//   return `Happy holidays, ${name}!`
+// }
+// 
+// function happyCustomHolidayTo(holiday, name) {
+//   return `Happy ${holiday}, ${name}!`
+// }
 
 // function holidayCountdown(holiday, days) {
 //   return `It's ${days} days until ${holiday}!`
