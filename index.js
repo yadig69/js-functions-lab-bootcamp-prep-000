4b341e50fca0aac168095d9a88e8c5226115b312
Gregory Singleton <yadig69design@gmail.com>
2020-06-15 03:27:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var date = "july 3rd"
 console.log(`my birthday is ${date}`)
 console.log(`I have ${1 + 1} pets`)
 
-const happyHolidays = () => "Happy holidays!";
+const happyHoliday = () => "Happy holidays!";
 const happyHolidaysTo = (name) => `Happy holidays, ${name}!`;
 const happyHolidayTo = (holiday, name) => `Happy ${holiday}, ${name}!`;
 const holidayCountdown = (holiday, days) => `It's ${days} days until ${holiday}!`;
