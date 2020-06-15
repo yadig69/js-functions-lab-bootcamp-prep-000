75e40dbe2107eba1d337535b9e6a1882dce4a12c
Gregory Singleton <yadig69design@gmail.com>
2020-06-15 03:46:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ var date = "july 3rd"
 console.log(`my birthday is ${date}`)
 console.log(`I have ${1 + 1} pets`)
 
-const happyHoliday = () => "Happy holidays!";
-const happyHolidaysTo = (name) => `Happy holidays, ${name}!`;
-const happyHolidayTo = (holiday, name) => `Happy ${holiday}, ${name}!`;
-const holidayCountdown = (holiday, days) => `It's ${days} days until ${holiday}!`;
+// const happyHoliday = () => "Happy holidays!";
+// const happyHolidaysTo = (name) => `Happy holidays, ${name}!`;
+// const happyHolidayTo = (holiday, name) => `Happy ${holiday}, ${name}!`;
+// const holidayCountdown = (holiday, days) => `It's ${days} days until ${holiday}!`;
 
-// function happyHolidays() {
-//   console.log("Happy holidays!")
-// }
-//
-// function happyHolidaysTo(name) {
-//   return `Happy holidays, ${name}!`
-// }
-//
-// function happyCustomHolidayTo(holiday, name) {
-//   return `Happy ${holiday}, ${name}!`
-// }
+function happyHolidays() {
+  console.log("Happy holidays!")
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyCustomHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
 
 // function holidayCountdown(holiday, days) {
 //   return `It's ${days} days until ${holiday}!`
