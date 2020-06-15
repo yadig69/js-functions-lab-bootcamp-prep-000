661472a3b84290b77418ce454beba42a8d29fa70
Gregory Singleton <yadig69design@gmail.com>
2020-06-15 03:06:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 var date = "july 3rd"
 console.log(`my birthday is ${date}`)
 console.log(`I have ${1 + 1} pets`)
+
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return "Happy holidays, ${name}"
+}
+
